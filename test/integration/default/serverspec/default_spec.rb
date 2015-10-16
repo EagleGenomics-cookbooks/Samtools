b030ed372d7fe2df35484c958bbef45d9fc07afa
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-10-16 14:49:36 +0100

    Removed unrelated tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -13,15 +13,3 @@ describe file(file_path) do
   it { should be_file }
   it { should be_executable }
 end
-
-# describe command('rsem-bam2readdepth') do
-#   its(:stdout) { should contain('Usage: ') }
-# end
-
-# describe command('rsem-tbam2gbam') do
-#   its(:stdout) { should contain('Usage: ') }
-# end
-
-# describe command('rsem-prepare-reference') do
-#   its(:stderr) { should contain('Invalid number of arguments') }
-# end
