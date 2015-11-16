f7c1beecdcee5f013fe3673601fe4b267d2c34a3
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2015-11-16 16:51:02 +0000

    Added version and path tests.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -13,3 +13,11 @@ describe file(file_path) do
   it { should be_file }
   it { should be_executable }
 end
+
+describe command("#{file_path} --version") do
+  its(:stdout) { should contain("samtools " + ENV['Samtools_VERSION']) }
+end
+
+describe command("which samtools") do
+  its(:exit_status) { should eq 0 }
+end
