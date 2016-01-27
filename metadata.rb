311ad17665d74bddde30be005d9bb7732c945ef2
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-27 10:54:59 +0000

    Standardising license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'Samtools'
 maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'apache2'
+license 'Apache v2.0'
 description 'Installs/Configures Samtools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
