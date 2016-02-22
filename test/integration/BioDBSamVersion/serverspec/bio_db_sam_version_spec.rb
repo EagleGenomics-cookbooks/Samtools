e62b9bc5d54ec4c31ec0671fdaf3197dec60b920
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-02-22 13:14:19 +0000

    Added link to Joshua Timberman's code blog to document the require_relative lines when using multiple test suites

--- test/integration/BioDBSamVersion/serverspec/bio_db_sam_version_spec.rb
+++ test/integration/BioDBSamVersion/serverspec/bio_db_sam_version_spec.rb
@@ -1,2 +1,3 @@
+# See http://jtimberman.housepub.org/blog/2014/12/31/quick-tip-serverspec-spec-helper-in-test-kitchen/
 require_relative '../../../kitchen/data/spec_helper'
 require_relative '../../../kitchen/data/samtools_spec'
